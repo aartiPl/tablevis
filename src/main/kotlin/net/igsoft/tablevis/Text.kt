3a5a4c49c2eb41e7494ce505e6948df12e745d9d
Marcin <aarti@interia.pl>
2021-12-29 16:20:14 +0100

    Improved text splitting.

--- src/main/kotlin/net/igsoft/tablevis/Text.kt
+++ src/main/kotlin/net/igsoft/tablevis/Text.kt
@@ -19,12 +19,15 @@ object Text {
      * If splitting on whitespace is not possible "-" is added on split point.
      */
     fun splitLineTextually(line: String, width: Int): List<String> {
-        //1. line.length < textWidth
+        //1. line.length < textWidth => take the whole line
         if (line.length <= width) {
             return listOf(line)
         }
 
         val newLines = mutableListOf<String>()
+        //NOTE: Algorithm
+        // width + 1 may contain space; that's correct splitting point - line will have size of width
+        // max((4 * width / 5), width - 7) - more than 7 chars in lookup range is too much - text doesn't look good
         val lookupRange = width + 1 downTo max((4 * width / 5), width - 7)
 
         var rest = line
@@ -35,8 +38,7 @@ object Text {
                 continue
             }
 
-            //2. Between cell's textWidth  + 1 (since if the next char is whitespace then splitting is also okay to split)
-            //   and 4/5 of line length (lastIndexOf) is whitespace
+            //2. There is a whitespace in lookupRange => split by whitespace
             for (i in lookupRange) {
                 if (rest[i - 1].isWhitespace()) {
                     newLines += rest.substring(0, i - 1).trimEnd()
@@ -45,7 +47,7 @@ object Text {
                 }
             }
 
-            //3. put dash in split point and continue
+            //3. There are only chars in lookupRange => split the word with dash and continue
             newLines += rest.substring(0, width - 1) + "-"
             rest = rest.substring(width - 1)
         }
